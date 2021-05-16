608fed5ecbd4210e4f844e6eb6cb619cbb4851e0
Leo <efrenn98@gmail.com>
2021-05-15 18:13:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // Code your solution here
 function findMatching(array, string){
-    let drivers= array.filter(n=>{
-        if( n === string){
-            return n
-        }
-    })
+    let drivers= array.filter(n=>
+        n.toLowerCase()=== string.toLowerCase()
+         
+        )
+        return drivers
 }
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
